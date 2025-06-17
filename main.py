8637c4dc2e24a8e65826df5dcedd8063deb5063a
DarkFeed2005 <kalanayasassrisathruwan123@gmail.com>
2025-06-17 23:30:12 +0530

    main.py

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ app.geometry("1000x680+325+175")
 app.title("MOH Login")
 app.resizable(0,0)
 
+
+side_img_data = Image.open("side-img.png")
+email_icon_data = Image.open("email-icon.png")
+password_icon_data = Image.open("password-icon.png")
+google_icon_data = Image.open("google-icon.png")
+
 app.mainloop()
